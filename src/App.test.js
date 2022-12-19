f8658f1d859d8729658eff5efd1218a2ce8af78d
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2022-12-19 21:43:28 +0300

    created the structure for the project

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 
+
 test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
