a183e2a7ffe86253cf2843bcf99753c658824e3f
MhamadR <mhamadraslan@gmail.com>
2022-12-21 20:52:48 +0200

    Feat browserRouter as per #2
    Add route structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Footer from './components/Footer/Footer';
 import './App.css';
@@ -6,8 +7,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        {/* Insert navbar Component here */}
+        <Routes>
+          {/* When components are created, insert them in element */}
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element="" />
+          <Route path="/about" element="" />
+          <Route path="/contact" element="" />
+          <Route path="/login" element="" />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Subscribe } from './Subscribe.svg';
 import { ReactComponent as Twitter } from './Twitter.svg';
 import { ReactComponent as Facebook } from './Facebook.svg';
@@ -28,33 +28,22 @@ function Footer() {
         </div>
         <div className="w-4/12">
           <div className="flex justify-evenly">
-            <a href="/home">Home</a>
-            <a href="/blogs">Blogs</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-          </div>
-          <div>
-            <Twitter />
-            <Facebook />
-            <Github />
-          </div>
-          {/* <div>
             <NavLink to="/home">Home</NavLink>
             <NavLink to="/blogs">Blogs</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/contact">Contact</NavLink>
           </div>
           <div>
-            <NavLink to="">
-              <img src={Twitter} alt="Twitter icon" />
-            </NavLink>
-            <NavLink to="">
-              <img src={Facebook} alt="Facebook icon" />
-            </NavLink>
-            <NavLink to="">
+            <a href="#twitter">
+              <Twitter />
+            </a>
+            <a href="#facebook">
+              <Facebook />
+            </a>
+            <a href="https://github.com/rozhiar-tech/Happy">
               <Github />
-            </NavLink>
-          </div> */}
+            </a>
+          </div>
         </div>
       </div>
     </footer>
