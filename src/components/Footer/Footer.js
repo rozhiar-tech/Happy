789d38be8928ffaa88e4d5ec8809d1f69ecd8458
MhamadR <mhamadraslan@gmail.com>
2022-12-22 15:49:31 +0200

    Style footer as per #2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,45 +3,73 @@ import { NavLink } from 'react-router-dom';
 import { ReactComponent as Subscribe } from './Subscribe.svg';
 import { ReactComponent as Twitter } from './Twitter.svg';
 import { ReactComponent as Facebook } from './Facebook.svg';
-import { ReactComponent as Github } from './Github.svg';
+import { ReactComponent as Google } from './Google.svg';
 
 function Footer() {
   const [email, setEmail] = useState('');
 
   return (
-    <footer className="p-4 w-screen">
-      <div className="flex justify-between items-center w-full">
-        <div className="w-4/12">
-          <h2 className="text-start">Subscribe</h2>
-          <p className="text-start">We’ll never spam you or share your email</p>
-          <div className="flex">
+    <footer className="py-4 px-16 w-screen h-60 bg-lavender-indigo absolute bottom-0 left-0">
+      <div className="flex justify-between items-center w-full h-full">
+        <div className="flex flex-col justify-evenly w-4/12 h-full">
+          <div>
+            <h2 className="text-start font-medium text-4xl mb-4">Subscribe</h2>
+            <p className="text-start text-xl text-black/[0.65]">
+              We’ll never spam you or share your email
+            </p>
+          </div>
+          <div className="flex border-2 rounded border-pale-spring-bud w-fit">
             <input
               type="text"
+              className="text-xl pl-4 focus:outline-lavender-indigo"
               placeholder="Enter your e-mail"
               value={email}
               onChange={(event) => setEmail(() => event.target.value)}
             />
-            <div>
+            <div className="flex justify-center items-center cursor-pointer bg-vodka border-l-2 border-pale-spring-bud w-[60px] h-[60px]">
               <Subscribe />
             </div>
           </div>
         </div>
-        <div className="w-4/12">
-          <div className="flex justify-evenly">
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/blogs">Blogs</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+        <div className="flex flex-col justify-evenly items-center w-4/12 h-full">
+          <div className="flex justify-between pt-4 text-xl w-full text-black/[0.65]">
+            <NavLink to="/home" className="hover:text-black focus:text-black">
+              Home
+            </NavLink>
+            <NavLink to="/blogs" className="hover:text-black focus:text-black">
+              Blogs
+            </NavLink>
+            <NavLink to="/about" className="hover:text-black focus:text-black">
+              About
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="hover:text-black focus:text-black"
+            >
+              Contact
+            </NavLink>
           </div>
-          <div>
-            <a href="#twitter">
+          <div className="flex justify-evenly  py-4 w-6/12">
+            <a
+              href="#twitter"
+              className="hover:scale-110 focus:scale-110"
+              onClick={(e) => e.preventDefault()}
+            >
               <Twitter />
             </a>
-            <a href="#facebook">
+            <a
+              href="#facebook"
+              className="hover:scale-110 focus:scale-110"
+              onClick={(e) => e.preventDefault()}
+            >
               <Facebook />
             </a>
-            <a href="https://github.com/rozhiar-tech/Happy">
-              <Github />
+            <a
+              href="#google"
+              className="hover:scale-110 focus:scale-110"
+              onClick={(e) => e.preventDefault()}
+            >
+              <Google />
             </a>
           </div>
         </div>
