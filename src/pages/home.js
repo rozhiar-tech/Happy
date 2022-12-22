26978d00b05cac7b552da85efe35c3bb3d03faa2
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2022-12-22 16:47:56 +0300

    added the image and the text

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import FirstSection from "../components/firstSection";
 
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <FirstSection/>
     </div>
   );
 }
