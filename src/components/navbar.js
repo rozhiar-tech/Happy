e88b1a3182dd3148be9197b8ecc8b11be1232b22
ramagitup <ro21090@auis.edu.krd>
2022-12-24 01:23:09 +0300

    fixed a lottt of bugs

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,24 @@
 import React from "react";
 import './styles/navbar.css'
-//import logo from "./public/images/amro.png"//
+
 
 function navbar() {
   return (
-    <div class="navbar">
-  <a href="#home">Home</a>
-  <a href="#blogs">Blogs</a>
-  <div class="dropdown">
-    <button class="dropbtn">About 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <a href="#contact us">Contact Us</a>
-  <image src="logo" alt="" />
+    <div className="navbar">
+        <a href="#home">Home</a>
+        <a href="#blogs">Blogs</a>
+        <div className="dropdown">
+            <button className="dropbtn" type="button">About 
+            <i className="fa fa-caret-down"/>
+            </button>
+            <div className="dropdown-content">
+            <button href="#" type="button">Link 1</button>
+            <button href="#" type="button">Link 2</button>
+            <button href="#" type="button">Link 3</button>
+            </div>
+        </div> 
+        <a href="#contact us">Contact Us</a>
+        <image src="logo" alt="" />
 </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FirstSection from "../components/firstSection";
 import Footer from "../components/Footer/Footer";
-import navbar from "../components/navbar"
+import Navbar from "../components/navbar"
 
 
 function Home() {
