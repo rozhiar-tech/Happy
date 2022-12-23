7e5e215ef945f3ad60ee97917dd267230aff60e9
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2022-12-23 19:10:33 +0300

    added test boiler plates for the project

--- src/components/firstSection.js
+++ src/components/firstSection.js
@@ -10,6 +10,9 @@ const FirstSection = () => {
         <div  className="ml-auto m-auto">
              <p  className="  text-xl font-semibold">We are Here To Make you <span className="happy">Happy</span></p> 
              <p  className="  text-4xl font-bold">Happy</p> 
+             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" type="button">
+                Book an Appointment 
+            </button>
         </div>
              <img src={therapy} alt="therapy" className="mr-auto"/>
         </div>
