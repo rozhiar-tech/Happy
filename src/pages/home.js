f0e033266ccbb70a715165a561c7cffb0b66df22
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-01-02 21:34:07 +0300

    .

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import FirstSection from '../components/Home/firstSection';
 import SecondSection from '../components/Home/secondSection';
-import FifthSection from '../components/Home/FifthSection';
 
 function Home() {
   return (
     <div>
       <FirstSection />
       <SecondSection />
-      <FifthSection />
     </div>
   );
 }
