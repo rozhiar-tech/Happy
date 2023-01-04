4b87bf304bc688600170489c4a2250c36b6db4f7
ramagitup <ro21090@auis.edu.krd>
2023-01-05 01:27:14 +0300

    Changed Some Positioning.

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FirstSection from '../components/Home/firstSection';
 import SecondSection from '../components/Home/secondSection';
 import ThirdSection from '../components/Home/thirdSection';
+import FourthSection from "../components/Home/fourthsection";
 import FifthSection from '../components/Home/FifthSection';
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <FirstSection />
       <SecondSection />
       <ThirdSection />
+      <FourthSection />
       <FifthSection />
     </div>
   );
