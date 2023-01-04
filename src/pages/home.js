caaf9623a8820bb94ad16a74e8ee766d38e61518
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-01-04 14:03:27 +0300

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import FirstSection from '../components/Home/firstSection';
 import SecondSection from '../components/Home/secondSection';
+import ThirdSection from '../components/Home/thirdSection';
+import FifthSection from '../components/Home/FifthSection';
 
 function Home() {
   return (
     <div>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
+      <FifthSection />
     </div>
   );
 }
