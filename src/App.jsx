ba60a87a9f836d9b48c34d60207bec8bc730a7bd
Zoriana <zoriana.shatkovska@gmail.com>
2023-01-05 21:27:00 +0300

    responsive about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import About from './pages/about'
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Home/navbar';
 import './App.css';
@@ -16,7 +17,7 @@ function App() {
           {/* When components are created, insert them in element */}
           <Route path="/" element={<Home />} />
           <Route path="/blogs" element="" />
-          <Route path="/about" element="" />
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element="" />
           <Route path="/login" element="" />
         </Routes>
