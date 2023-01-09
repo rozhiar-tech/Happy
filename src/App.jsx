a4589ee20f32ad0192d52965b14dde99ea2aab47
MhamadR <mhamadraslan@gmail.com>
2023-01-09 18:46:38 +0200

    Refactor imports and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer";
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact/Contact';
-import Payment from "./pages/Payment/Payment";
+import Payment from './pages/Payment/Payment';
+import AfterPayment from './pages/AfterPayment/AfterPayment';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
-import About from './pages/about'
+import About from './pages/About/About';
 import Team from './pages/Team/team';
 
 function App() {
@@ -22,8 +23,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/payment" element={<Payment />} />
-          <Route path="/login" element=<Login/>  />
-          <Route path="/signup" element=<Signup/> />
+          <Route path="/afterpayment" element={<AfterPayment />} />
+          <Route path="/login" element=<Login /> />
+          <Route path="/signup" element=<Signup /> />
           <Route path="/team" element={<Team />} />
         </Routes>
         <Footer />
