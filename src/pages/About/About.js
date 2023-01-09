de5b4284067987e8ee185d42e553adea32eb2723
MhamadR <mhamadraslan@gmail.com>
2023-01-09 18:44:05 +0200

    Fix About.js
    Update About.js with about.js and fixed imports

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,12 +5,12 @@ const About = () => {
   return (
     <div className="flex flex-col">
       <div className="my-20 max-w-6xl mx-auto px-4">
-        <div className=" lg:text-left  sm:text-center">
-          <h1 className=" mb-2 text-5xl mb-16">Happy!</h1>
-          {/* <h4 className="text-gray-400 mb-16">some cool one liner !</h4> */}
+        <div className=" md:text-left text-center">
+          <h1 className=" mb-2 text-5xl ">HEALING!</h1>
+          <h4 className="text-gray-400 mb-16">some cool one liner !</h4>
         </div>
         <p className="text-gray-700 text-lg leading-8 text-justify">
-          At Happy, we believe there is a better way to do things. A more
+          At Healing, we believe there is a better way to do things. A more
           valuable way where customers are earned rather than bought. We&#39;re
           obsessively passionate about it, and our mission is to help people
           achieve it. We focus on search engine optimization. It&#39;s one of
@@ -24,14 +24,14 @@ const About = () => {
         <img
           src={rectangle}
           alt="founder"
-          className="md:w-[400px] sm:w-[312px] lg:mx-0 md:mx-auto sm:mx-auto "
+          className="md:w-[400px] w-[312px] lg:mx-0 mb-2 mx-auto "
         />
-        <div className="max-w-2xl lg:mx-0 md:mx-auto sm:mx-auto lg:px-2 md:px-4 sm:px-6">
-          <h1 className="mb-4 text-4xl text-gray-700 sm:text-center">
+        <div className="max-w-2xl lg:mx-0 md:mx-auto sm:mx-auto  px-4">
+          <h1 className="mb-4 text-4xl text-gray-700 text-center">
             Our Founding
           </h1>
           <p className="text-gray-900 text-justify text-lg leading-7 lg:pr-6">
-            Happy was founded by Payam Abubakr in 2021. It was called Happy
+            Healing was founded by Payam Abubakr in 2021. It was called Healing
             Online, and started as a blog and an online community where some of
             the world&#39;s therapists shared their research and ideas. We
             launched the Beginner&#39;s Guide to Therapy and our first study,
