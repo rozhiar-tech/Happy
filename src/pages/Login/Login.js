2dc17b11a66f0f43d8d84ed133b364f2ad877db3
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-01-12 08:52:55 +0300

    fixed some ui

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -20,6 +20,7 @@ function Login() {
       setError("");
       setLoading(true);
       await signInWithEmailAndPassword(auth, email, password);
+      
       navigate("/");
     } catch {
       setError("Failed to log in");
@@ -32,45 +33,33 @@ function Login() {
   };
 
   return (
-    <div className="h-screen bg-vodka flex justify-evenly  ">
+    <div className="h-screen bg-vodka flex justify-evenly ">
     
 
-      <div className="bg-white h-96 w-96 rounded-lg m-24 shadow-lg p-10 flex justify-between">
-      <form onSubmit={handleSubmit} className="flex flex-col  justify-center ">
-        <label 
-        htmlFor="email"
-        className="flex flex-col text-center mb-10"
-        >        
-          Email
-          <input
+      <div className="bg-white h-[500px] w-[470px] rounded-lg m-24 shadow-lg px-11 flex justify-between  ">
+      <form onSubmit={handleSubmit} className="flex flex-col  justify-evenly">
+      <input
+            placeholder="Email"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className=" w-96 h-[75px] p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md "
           />
-          
-        </label>
-        <label
-        className="flex flex-col text-center  mb-10"
-
-        htmlFor="password"
-        >
-          Password
-          <input
+        <input
+            placeholder="Password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="block w-full p-4 text-gray-900 border border-white rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="w-96 h-[75px] p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md"
 
           />
-        </label>
       {error && <p>{error}</p>}
        <div className="flex justify-between">
 
-       <button type="submit" disabled={loading} className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white  py-2 px-4 rounded w-36 mr-3">
+       <button type="submit" disabled={loading} className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white  py-2 px-4 rounded mr-3 h-[60px] w-[186px]">
           Login
         </button>
-        <button type="button" onClick={senUserToSignUp} disabled={loading} className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white  py-2 px-4 rounded w-36">
+        <button type="button" onClick={senUserToSignUp} disabled={loading} className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white  py-2 px-4 rounded h-[60px] w-[186px]">
           SignUp
         </button>
         </div>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useNavigate  } from "react-router-dom";
 import { getAuth ,createUserWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase/firebaseinit";
-import therapy from "./therapy.svg";
+import signupImage from "./SignupImage.svg";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
 
 
 const auth = getAuth(app);
@@ -15,6 +16,7 @@ function SignUp(){
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const db = getFirestore(app);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,11 +24,17 @@ function SignUp(){
         setError("");
         setLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
+        .then(async(userCredential) => {
             // Signed in 
 
             const {user} = userCredential;
             console.log(user)
+            const docRef = await addDoc(collection(db, "cities"), {
+                name: "Tokyo",
+                country: "Japan"
+              });
+              console.log("Document written with ID: ", docRef.id);
+            
 
             navigate("/");
             // ...
@@ -54,52 +62,49 @@ function SignUp(){
         setLoading(false);
     };
 
- 
-
+    const senToLogin = () => {
+        navigate("/login");
+    };
+    
+    
     return (
         <div className="h-screen bg-vodka flex justify-evenly">
+        <img src={signupImage} alt="therapy" className="h-3/6 mt-14 "/>
             
 
-            <div className="bg-white h-96 w-96 rounded-lg m-24 shadow-lg p-10">
-            <form onSubmit={handleSubmit} className="flex flex-col  justify-center ">
-                <label 
-                htmlFor="email"
-                className="flex flex-col text-center mb-10"
-                >        
-                    Email
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    />
-                    
-                </label>
-                <label
-                className="flex flex-col text-center  mb-10"
+            <div className="bg-white h-[500px] w-[470px] rounded-lg m-24 shadow-lg px-11 flex justify-between ">
+            <form onSubmit={handleSubmit} className="flex flex-col  justify-evenly">
+            <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className=" w-96 h-[75px] p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md "
+          />
+        <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-96 h-[75px] p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md"
 
-                htmlFor="password"
-                >
-                    Password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    />
-                </label>
-                {error && <p>{error}</p>}
+          />
+      {error && <p>{error}</p>}
+       <div className="flex justify-between">
 
-                <button
-                type="submit"
-                className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white rounded p-4 w-1/2 self-center"
-                disabled={loading}
-                >
-                    Sign Up
-                </button>
+       <button type="button" onClick={senToLogin} disabled={loading} className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white  py-2 px-4 rounded mr-3 h-[60px] w-[186px]">
+          Login
+        </button>
+        <button
+            type="submit"
+            className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white rounded p-4 w-1/2 self-center"
+            disabled={loading}
+            >
+            Sign Up
+         </button>
+        </div>
             </form>
             </div>
-      <img src={therapy} alt="therapy" className="h-3/6 mt-14 "/>
 
         </div>
     )
