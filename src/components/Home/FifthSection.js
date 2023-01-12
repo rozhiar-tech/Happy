4698e94e69805484ec10438e90a9e3eaf2463bf4
MhamadR <mhamadraslan@gmail.com>
2023-01-12 22:39:45 +0200

    Style FifthSection as per #35

--- src/components/Home/FifthSection.js
+++ src/components/Home/FifthSection.js
@@ -16,7 +16,7 @@ function FifthSection() {
           <p className="text-[2.2rem] leading-[3.3rem]">10$</p>
           <NavLink
             to="/contact"
-            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
+            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded ease-in-out duration-300 px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
           >
             PURCHASE
           </NavLink>
@@ -26,7 +26,7 @@ function FifthSection() {
           <p className="text-[2.2rem] leading-[3.3rem]">40$</p>
           <NavLink
             to="/contact"
-            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
+            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded ease-in-out duration-300 px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
           >
             PURCHASE
           </NavLink>
@@ -36,13 +36,13 @@ function FifthSection() {
           <p className="text-[2.2rem] leading-[3.3rem]">70$</p>
           <NavLink
             to="/contact"
-            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
+            className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded ease-in-out duration-300 px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
           >
             PURCHASE
           </NavLink>
         </div>
       </div>
-      <div className="flex flex-col justify-center gap-4 border border-vodka rounded-2xl shadow-vodka/25 shadow-xl mt-[4.4rem] text-center">
+      <div className="flex flex-col justify-center gap-4 border border-vodka rounded-2xl shadow-vodka/25 shadow-xl mt-[4.4rem] py-[4.4rem] text-center">
         <h2 className="text-[3.12rem] leading-[4.7rem]">
           ARE YOU A COUNSELOR?
         </h2>
@@ -53,7 +53,7 @@ function FifthSection() {
         </p>
         <NavLink
           to="about/counselor"
-          className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded px-[2rem] py-[0.75rem] max-w-full w-fit self-center"
+          className="text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded px-[2rem] ease-in-out duration-300 py-[0.75rem] max-w-full w-fit self-center"
         >
           LEARN MORE
         </NavLink>
