e09c62ce23e2f1644675b91b174a3f456664bdbf
MhamadR <mhamadraslan@gmail.com>
2023-01-12 19:20:24 +0200

    Style ThirdSection Home component as per #35

--- src/components/Home/ThirdSection/ThirdSection.js
+++ src/components/Home/ThirdSection/ThirdSection.js
@@ -6,38 +6,40 @@ import video from './video-call.svg';
 
 const ThirdSection = () => {
   return (
-    <div className="pt-20 pb-16  max-w-7xl mx-auto">
-      <h2 className="mb-16 text-5xl sm:px-7">WE CAN COMMUNICATE THROUGH</h2>
-      <div className="cards grid lg:grid-cols-3 md:grid-cols-2 md:px-7 sm:grid-cols-1 sm:px-5 gap-6">
-        <div className="card-1 py-6 border border-gray-100  rounded-2xl shadow-xl">
-          <NavLink>
-            <img src={phone} alt="voiceCall" className="mx-auto mb-8" />
-            <h4 className=" text-4xl text-center mb-4">Voice Call</h4>
-            <p className="text-center mx-12 text-gray-500 leading-7">
-              Feeling ready to start a conversation? Give your therapist a voice
-              call and talk your heart out!
-            </p>
-          </NavLink>
-        </div>
-        <div className="card-2 py-6 border border-gray-100  rounded-2xl shadow-xl">
-          <NavLink>
-            <img src={chat} alt="chat" className="mx-auto mb-8" />
-            <h4 className="text-4xl text-center mb-4">Chat</h4>
-            <p className="text-center mx-12 text-gray-500 leading-7">
-              Need to talk to someone? Come have a chat with your therapist!
-            </p>
-          </NavLink>
-        </div>
-        <div className="card-3 py-6 border border-gray-100  rounded-2xl shadow-xl">
-          <NavLink>
-            <img src={video} alt="videoCall" className="mx-auto mb-8" />
-            <h4 className="text-4xl text-center mb-4">Video Call</h4>
-            <p className="text-center mx-12 text-gray-500 leading-7">
-              For better experience therapists recommend video calls, but always
-              remember that its a choice!
-            </p>
-          </NavLink>
-        </div>
+    <div className="px-4 sm:px-16 lg:px-[10.25rem] w-screen py-24 break-words w-screen">
+      <h2 className="mb-16 text-5xl">WE CAN COMMUNICATE THROUGH</h2>
+      <div className="flex flex-row flex-wrap gap-4 justify-evenly items-center text-center my-7">
+        <NavLink className="flex flex-col items-center gap-4 border border-vodka rounded-2xl shadow-xl shadow-vodka/25 w-[23rem] max-w-full min-h-[21.875rem] h-fit pt-[3rem]">
+          <img
+            src={phone}
+            alt="Voice call"
+            className="w-[5.625rem] h-[5.625rem]"
+          />
+          <h4 className="text-4xl w-fit max-w-full">Voice Call</h4>
+          <p className="text-gray-500 leading-7 px-2 sm:px-12 w-fit max-w-full">
+            Feeling ready to start a conversation? Give your therapist a voice
+            call and talk your heart out!
+          </p>
+        </NavLink>
+        <NavLink className="flex flex-col items-center gap-4 border border-vodka rounded-2xl shadow-xl shadow-vodka/25 w-[23rem] max-w-full min-h-[21.875rem] h-fit pt-[3rem]">
+          <img src={chat} alt="Chat" className="w-[5.625rem] h-[5.625rem]" />
+          <h4 className="text-4xl w-fit max-w-full">Chat</h4>
+          <p className="text-gray-500 leading-7 px-2 sm:px-12 w-fit max-w-full">
+            Need to talk to someone? Come have a chat with your therapist!
+          </p>
+        </NavLink>
+        <NavLink className="flex flex-col items-center gap-4 border border-vodka rounded-2xl shadow-xl shadow-vodka/25 w-[23rem] max-w-full min-h-[21.875rem] h-fit pt-[3rem]">
+          <img
+            src={video}
+            alt="Video call"
+            className="w-[5.625rem] h-[5.625rem]"
+          />
+          <h4 className="text-4xl w-fit max-w-full">Video Call</h4>
+          <p className="text-gray-500 leading-7 px-2 sm:px-12 w-fit max-w-full">
+            For better experience therapists recommend video calls, but always
+            remember that its a choice!
+          </p>
+        </NavLink>
       </div>
     </div>
   );
