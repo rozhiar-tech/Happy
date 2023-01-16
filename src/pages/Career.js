e8343f01f8ff0e8e0355a936a982332cde5ce935
MhamadR <mhamadraslan@gmail.com>
2023-01-16 15:30:33 +0200

    Add CareerPart2 to Career page

---
+++ src/pages/Career.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CareerPart1 from '../components/Career/CareerPart1';
+import CareerPart2 from '../components/Career/CareerPart2';
+
+function Career() {
+  return (
+    <div className="w-full">
+      <CareerPart1 />
+      <CareerPart2 />
+    </div>
+  );
+}
+
+export default Career;
