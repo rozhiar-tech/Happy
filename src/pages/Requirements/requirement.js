ea4191210b95474c3aa5e6a394c134528d1d1858
ramagitup <ro21090@auis.edu.krd>
2023-01-16 14:36:45 +0300

    Some Changes

--- src/pages/Requirements/requirement.js
+++ src/pages/Requirements/requirement.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import "./requirement.css";
 import { useNavigate } from 'react-router-dom';
-import image from "./requirementimg.jpg";
+import image from "./requirements_therapy.svg";
 
-const Issue = () => {
+const Issue = () => { 
   const navigate2 = useNavigate();
   return (
     <div className='requirements'>
-    <div className="text">
       <img alt="Therapy" src={image} className="image1"/>
+    <div className="text-new">
       <h3 className="text1">Why work with Healing?</h3>
-      <h4 className='text2'>Reliable Income
-      <section className='text5'>Over 10,000 people sign up on 
+      <h4 className='text2'>Reliable Income</h4>
+      <p className='text5'>Over 10,000 people sign up on 
       BetterHelp every day looking for a counselor to help with life’s challenges. 
-      BetterHelp can be your main source of income (full time) or a supplement to your current work.</section>
-      </h4>
-    </div>
-    <div>
-      <h4 className='text6'>Focus on Counseling
-      <section className='text5'>No need to worry about costs from acquiring clients, billing, support or operations.
-         Let us handle the fees and paperwork so you can focus on what you do best!</section></h4>
-
-         <h4 className='text6'>Focus on Counseling
-      <section className='text5'>No need to worry about costs from acquiring clients, billing, support or operations.
-         Let us handle the fees and paperwork so you can focus on what you do best!</section></h4>
-    </div>
-      <div>
+      BetterHelp can be your main source of income (full time) or a supplement to your current work.</p>
+      <h4 className='text6'>Focus on Counseling</h4>
+      <p className='text5'>No need to worry about costs from acquiring clients, billing, support or operations.
+         Let us handle the fees and paperwork so you can focus on what you do best!</p>
+         <h4 className='text6'>Focus on Counseling</h4>
+      <p className='text5'>No need to worry about costs from acquiring clients, billing, support or operations.
+         Let us handle the fees and paperwork so you can focus on what you do best!</p>
+         <div>
       <h3 className="text7">Requirements</h3>
         <ul className='important'>
           <li>Licensed by a State Board to provide counseling</li>
@@ -37,12 +32,13 @@ const Issue = () => {
       </div>
       <button
             id='button2'
-            type="submit"
+            type='button'
             className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white rounded px-4 py-2"
             onClick={() => navigate2('/')}
           >
             GET STARTED
           </button>
+    </div>
     </div>
   );
 };
