65bdd817646d7b6439621013080757d17d061351
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-01-16 14:11:00 +0300

    update normal user

--- src/pages/editProfile/normalUser.js
+++ src/pages/editProfile/normalUser.js
@@ -1,7 +1,20 @@
-import React from "react";
 
+import { useState } from "react";
 
-export default function NormalUser() {
+ function NormalUser() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmEmail, setConfirmEmail] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [birthDate, setBirthDate] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(firstName, lastName, email, password, confirmEmail, confirmPassword, birthDate);
+    }
+    
     return (
         <div className="h-screen w-screen bg-slate-300  ">
             <div className="flex flex-col items-center">
@@ -11,26 +24,37 @@ export default function NormalUser() {
                         <img className="h-40 w-40 rounded-full" src="https://profile-icon-png-image-free-download-searchpngcom-profile-icon-png-673_673.png" alt="profile" />
                     </div>
                     <div className="flex flex-col gap-4">
-                        <form className="flex flex-col gap-4">
-                            <label className="text-white">Name</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white">Email</label>
-                            <input className="border-2 border-white rounded-md" type="email" />
-                            <label className="text-white">Phone Number</label>
-                            <input className="border-2 border-white rounded-md" type="tel" />
-                            <label className="text-white">Address</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white">City</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white">State</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white">Country</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white" onChange={}>Zip Code</label>
-                            <input className="border-2 border-white rounded-md" type="text" />
-                            <label className="text-white">Date of Birth</label>
-                            <input className="border-2 border-white rounded-md" type="date" />
-                            
+                       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                            <input type="text" placeholder="First Name" className="h-10 w-80 rounded-lg" 
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+
+                            />
+                            <input type="text" placeholder="Last Name" className="h-10 w-80 rounded-lg"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            />
+                            <input type="text" placeholder="Birth Date" className="h-10 w-80 rounded-lg"
+                            value={birthDate}
+                            onChange={(e) => setBirthDate(e.target.value)}
+                            />
+                            <input type="text" placeholder="Email" className="h-10 w-80 rounded-lg"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input type="text" placeholder="Confirm Email" className="h-10 w-80 rounded-lg"
+                            value={confirmEmail}
+                            onChange={(e) => setConfirmEmail(e.target.value)}
+                            />
+                            <input type="text" placeholder="Password" className="h-10 w-80 rounded-lg"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <input type="text" placeholder="Confirm Password" className="h-10 w-80 rounded-lg"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
+                            <button type="submit" className="h-10 w-80 rounded-lg bg-red-600 text-white">Save</button>
                         </form>
                     </div>
                 </div>
@@ -40,3 +64,5 @@ export default function NormalUser() {
     );
     }
 
+
+export default NormalUser;
