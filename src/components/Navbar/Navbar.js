d92c4a4fe99891a9b448e790fa51a6e5c09a6f8d
MhamadR <mhamadraslan@gmail.com>
2023-01-25 23:55:03 +0200

    Fix navbar from @Riham code as per #66

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,139 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
-import logo from './logo.svg';
 
-function Navbar() {
+const Navbar = () => {
   return (
     <nav className="px-2 bg-white border-gray-200 dark:bg-vodka dark:border-gray-700">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <button type="button" href="#" className="flex items-center">
-          <img src={logo} className="h-11 mr-3 sm:h-10" alt=" Logo" />
-        </button>
-        <button
-          data-collapse-toggle="navbar-dropdown"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-dropdown"
-          aria-expanded="false"
+        <div
+          className="nav-elemnts hidden w-full md:block md:w-auto"
+          id="navbar-dropdown"
         >
-          <span className="sr-only">Open main menu</span>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col p-4 mt-4 border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700">
             <li>
-              <button
-                type="button"
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
-              >
+              <Link to="/" acriveStyle>
                 Home
-              </button>
+              </Link>
             </li>
             <li>
               <button
                 type="button"
-                id="dropdownNavbarLink"
-                data-dropdown-toggle="dropdownNavbar"
-                className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Dropdown{' '}
-                <svg
-                  className="w-5 h-5 ml-1"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  >
-                    s
-                  </path>
-                </svg>
+                <Link to="/team" acriveStyle>
+                  Team
+                </Link>
               </button>
-
-              <div
-                id="dropdownNavbar"
-                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  className="py-1 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
-                  <li>
-                    <button
-                      type="button"
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </button>
-                  </li>
-                </ul>
-                <div className="py-1">
-                  <button
-                    type="button"
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                  >
-                    Sign out
-                  </button>
-                </div>
+            </li>
+            <div className="dropdown">
+              <button type="button" className="dropbtn">
+                About
+              </button>
+              <div className="dropdown-content">
+                <Link to="about">Who are we</Link>
+                <Link to="career">Join our team</Link>
+                <Link to="requirements">Why to join our team</Link>
+                <Link to="payment">Payment Page</Link>
               </div>
+            </div>
+            <li>
+              <Link to="/contact" acriveStyle>
+                Contact Us
+              </Link>
             </li>
             <li>
-              <button
-                type="button"
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              <Link
+                to="/login"
+                className="bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 text-white font-bold py-2 px-4 rounded ease-in-out duration-300 w-fit max-w-full "
               >
-                Services
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </button>
+                Log in
+              </Link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
