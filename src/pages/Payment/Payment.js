1a0e98ddd7d82746c9f300c84acee3073bb859cd
MhamadR <mhamadraslan@gmail.com>
2023-01-25 23:25:36 +0200

    Fix payment page as per #34

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { datum } from './images.data';
 import Gratitude from '../../components/Gratitude';
-import { datum } from './image.data.';
 import './payment.css';
 
 const Payment = () => {
   const [isPayed, setIsPayed] = useState(false);
 
   const slideLeft = () => {
-    const slider2 = document.getElementById('slider1');
-    slider2.scrollLeft -= 500;
+    const slider = document.getElementById('slider');
+    slider.scrollLeft -= 380;
   };
 
   const slideRight = () => {
-    const slider3 = document.getElementById('slider1');
-    slider3.scrollLeft += 500;
+    const slider = document.getElementById('slider');
+    slider.scrollLeft += 380;
   };
 
   return (
@@ -22,53 +22,45 @@ const Payment = () => {
       {isPayed ? (
         <Gratitude message="Your Purchase Has Been Submitted." />
       ) : (
-        <div className="Payment">
-          <div className="text">
-            <h1 className="text-1">SELECT CARD</h1>
-            <h6 className="text-2">
-              Please select the card you want to buy the tickets with
-            </h6>
-            <div className="leader">
-              <div className="relative flex items-center">
-                <MdChevronLeft
-                  className="opacity-50 cursor-pointer hover:opacity-100"
-                  onClick={slideLeft}
-                  size={40}
+        <div className="py-24 px-4 sm:px-16 lg:px-[10.25rem] break-words w-screen">
+          <h2 className="text-[3.12rem] leading-[4.7rem]">Select Card</h2>
+          <p className="text-2xl leading-9 text-black/50">
+            Please select the card you want to buy the tickets with
+          </p>
+          <div className="flex gap-2 items-center px-4 sm:px-8 lg:px-[5.125] my-[4.4rem]">
+            <MdChevronLeft
+              className="text-lavender-indigo hover:text-wild-strawberry/70 hover:scale-95 focus:text-wild-strawberry/70 focus:scale-95 ease-in-out duration-300 cursor-pointer"
+              onClick={slideLeft}
+              size={100}
+            />
+            <div
+              id="slider"
+              className="flex gap-2 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+            >
+              {datum.map((item) => (
+                <img
+                  key={item.id}
+                  className="cursor-pointer inline-block hover:scale-105 focus:scale-105 ease-in-out duration-300"
+                  src={item.img}
+                  alt={item.img}
                 />
-                <div
-                  id="slider1"
-                  className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
-                >
-                  {datum.map((item) => (
-                    <img
-                      key={item.id}
-                      className="w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
-                      src={item.img}
-                      alt="/"
-                    />
-                  ))}
-                </div>
-
-                <MdChevronRight
-                  className="opacity-50 cursor-pointer hover:opacity-100"
-                  onClick={slideRight}
-                  size={40}
-                />
-              </div>
-              <h6 className="text3">
-                Click confirm to use the selected card to purchase 5 tickets for
-                10$
-              </h6>
-              <button
-                id="button"
-                type="submit"
-                className="bg-lavender-indigo hover:bg-wild-strawberry/70 focus:bg-wild-strawberry/70 hover:scale-95 focus:scale-95 text-white rounded px-4 py-2"
-                onClick={() => setIsPayed(true)}
-              >
-                CONFIRM PURCHASE
-              </button>
+              ))}
             </div>
+            <MdChevronRight
+              className="text-lavender-indigo hover:text-wild-strawberry/70 hover:scale-95 focus:text-wild-strawberry/70 focus:scale-95 ease-in-out duration-300 cursor-pointer"
+              onClick={slideRight}
+              size={100}
+            />
           </div>
+          <p className="capitalize text-4xl leading-[3.375rem]">
+            Click confirm to use the selected card to purchase 5 tickets for 10$
+          </p>
+          <input
+            type="button"
+            value="Confirm Purchase"
+            onClick={() => setIsPayed(() => true)}
+            className="relative left-1/2 -translate-x-1/2 uppercase text-2xl leading-[2.25rem] text-white bg-lavender-indigo hover:bg-wild-strawberry/70 hover:scale-95 focus:bg-wild-strawberry/70 focus:scale-95 rounded ease-in-out duration-300 px-[2rem] py-[0.75rem] max-w-full w-fit mt-[4.4rem]"
+          />
         </div>
       )}
     </div>
