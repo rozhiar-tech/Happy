52e05905bf5425d579c2ba2479d01975b77c50d1
MhamadR <mhamadraslan@gmail.com>
2023-01-25 23:24:13 +0200

    Change payment.data and add images

---
+++ src/pages/Payment/images.data.js
@@ -0,0 +1,42 @@
+import card1 from './card1.svg';
+import card2 from './card2.svg';
+import card3 from './card3.svg';
+
+export const datum = [
+  {
+    id: 1,
+    img: card1,
+  },
+  {
+    id: 2,
+    img: card2,
+  },
+  {
+    id: 3,
+    img: card3,
+  },
+  {
+    id: 4,
+    img: card1,
+  },
+  {
+    id: 5,
+    img: card2,
+  },
+  {
+    id: 6,
+    img: card3,
+  },
+  {
+    id: 7,
+    img: card1,
+  },
+  {
+    id: 8,
+    img: card2,
+  },
+  {
+    id: 9,
+    img: card3,
+  },
+];
